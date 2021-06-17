644215e5d00af49411746adfdfde89f500c3afa1
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-06-17 11:10:34 -0700

    c-hello files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Auth: Dismas Kipchumba
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line "\n"
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
