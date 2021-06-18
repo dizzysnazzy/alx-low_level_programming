8ade0557e5a5deb4b25c22d954f74b44b4210bf2
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-06-18 06:59:54 -0700

    modified 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,8 +7,7 @@
 
 	/**
 	 * main - Prints all single digit numbers of base 10 starting from 0.
-	 *
-	 *
+	 *Description: Prints all numbers of base 10, starting from 0.
 	 * Return: Always 0.
 	 */
 int main(void)
