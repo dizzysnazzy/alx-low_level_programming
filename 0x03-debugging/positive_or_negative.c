26c98c88273cc3457291138dc8659dc741513bfa
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-06-23 06:39:12 -0700

    positive or negative file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,13 +1,32 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Dismas Kipchumba
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #include "holberton.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	positive_or_negative();
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 
 	return (0);
 }
