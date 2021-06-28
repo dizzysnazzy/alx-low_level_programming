f8f876228d733e8c8be45ebe3ceb42ef03958a03
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-06-28 07:43:32 -0700

    modified files

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,45 +15,26 @@
  */
 int main(void)
 {
-	char password[84];
-	int index = 0, sum = 0, diff_half1, diff_half2;
+	int pass[100];
+	int index, sum, n;
 
-	srand(time(0));
+	sum = 0;
 
-	while (sum < 2772)
+	srand(time(NULL));
+
+	for (index = 0; index < 100; index++)
 	{
-		password[index] = 33 + rand() % 94;
-		sum += password[index++];
-	}
-
-	password[index] = '\0';
-
-	if (sum != 2772)
-	{
-		diff_half1 = (sum - 2772) / 2;
-		diff_half2 = (sum - 2772) / 2;
-		if ((sum - 2772) % 2 != 0)
-			diff_half1++;
-
-		for (index = 0; password[index]; index++)
+		pass[index] = rand() % 78;
+		sum += (pass[index] + '0');
+		putchar(pass[index] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			if (password[index] >= (33 + diff_half1))
-			{
-				password[index] -= diff_half1;
-				break;
-			}
-		}
-		for (index = 0; password[index]; index++)
-		{
-			if (password[index] >= (33 + diff_half2))
-			{
-				password[index] -= diff_half2;
-				break;
-			}
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
 		}
 	}
-
-	printf("%s", password);
 
 	return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,13 +11,21 @@
  */
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int i, index, len;
 
-	while (s[index++])
-		len++;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	len = i;
 
 	for (index = len - 1; index >= 0; index--)
+	{
 		_putchar(s[index]);
+	}
 
 	_putchar('\n');
 }
