f911bea2d6c672da1fef297dc9b751222a9e95ef
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-06-28 07:55:43 -0700

    modified and file

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,13 +15,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	int len, index;
 
-	while (src[index])
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (index = 0; index < len; index++)
 	{
 		dest[index] = src[index];
-		index++;
 	}
+	dest[index] = '\0';
 
 	return (dest);
 }
