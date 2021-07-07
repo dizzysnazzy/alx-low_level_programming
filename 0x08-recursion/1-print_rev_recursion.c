7a7f8ebd795e582192b800ed314125a32c9a1602
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-07 02:29:21 -0700

    All recursion files with scripts

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Dismas Kipchumba
+ */
+
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
