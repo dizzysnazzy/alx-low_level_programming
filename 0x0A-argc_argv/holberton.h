ffa5d2a0ea6ae9c12cd5233dab16aef4d903f33f
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-12 06:41:49 -0700

    All argc_argv files

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -5,5 +5,7 @@ int _putchar(char c);
 int main(void);
 
 int main(int __attribute__((__unused__)) argc, char *argv[]);
+int main(int argc, char __attribute__((__unused__)) *argv[]);
+int main(int argc, char *argv[]);
 
 #endif /* HOLBERTON_H */
