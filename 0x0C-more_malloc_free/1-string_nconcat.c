83846bed17fb2c64e8a2a1346daa7207dc86df8f
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-15 11:58:43 -0700

    All more malloc free files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,47 +3,35 @@
  * Auth: Dismas Kipchumba
  */
 
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * length - long of a string
- * @string: char
- * Return: length
- */
-unsigned int length(char *string)
-{
-	return ((*string == '\0') ? 0 : 1 + length(string + 1));
-}
-/**
- * string_nconcat - Entry point
- * @s1: char
- * @s2: char
- * @n: unsigned int
- * Return: Always 0 (Success)
+ * string_nconcat - concatenate two strings up to n bytes.
+ * @s1: source string
+ * @s2: string to truncate up to n bytes
+ * @n: number of bytes to truncate by
+ * Return: pointer to new buffer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *string;
-	unsigned int min_length;
-	unsigned int i, j, string_length;
+	char *p;
+	unsigned int s1count, s2count, sizeBuffer, i;
 
-	s1 = (s1 == NULL) ? ("") : s1;
-	s2 = (s2 == NULL) ? ("") : s2;
-	min_length = (n <= length(s2)) ? n : length(s2);
-	string_length = length(s1) + min_length + 1;
-
-	string = malloc(string_length * sizeof(*string));
-
-	if (string == NULL)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (s1count = 0; s1[s1count]; s1count++)
+		;
+	for (s2count = 0; s2[s2count]; s2count++)
+		;
+	s2count > n ? (s2count = n) : (n = s2count);
+	sizeBuffer = s1count + s2count + 1;
+	p = malloc(sizeBuffer * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; *(s1 + i) != '\0'; i++)
-		*(string + i) = *(s1 + i);
-
-	for (j = 0; *(s2 + j) != '\0' && i < (string_length - 1); i++, j++)
-		*(string + i) = *(s2 + j);
-
-	*(string + i) = '\0';
-	return (string);
+	for (i = 0; i < sizeBuffer - 1; i++)
+		i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+	p[sizeBuffer] = '\0';
+	return (p);
 }
