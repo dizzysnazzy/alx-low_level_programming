2506bdbccb17d6f8901835eaf8c72508b8ecb8b1
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-15 11:12:40 -0700

    All more malloc free files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,53 +4,62 @@
  */
 
 #include "holberton.h"
-#include <stdio.h>
-
+#include <stdlib.h>
 /**
- * string_nconcat - concatenate 2 strings, only n bytes of s2
- * @s1: string 1
- * @s2: string 2
- * @n: bytes to include of s2
- * Return: NULL if fail, else pointer to malloc memory
+ * _strlen - find length of a string
+ * @s: takes in a string
+ * Return: string length in i
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
-{
-	char *p;
-	int strlen1, i, c;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	strlen1 = (unsigned int)_strlen(s1);
-	p = malloc((strlen1 + n + 1) * sizeof(char));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0, c = 0; i < (strlen1 + n); i++)
-	{
-		if (i < strlen1)
-			p[i] = s1[i];
-		else
-			p[i] = s2[c++];
-	}
-	p[i] = '\0';
-
-	return (p);
-}
-
-/**
- * _strlen - find length of string
- * @s: string
- * Return: length of string
- */
-
 int _strlen(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		;
+	while (s[i] != '\0')
+		i++;
 	return (i);
+}
+/**
+ * string_nconcat - function that concats two strings
+ * @s1: takes in one string
+ * @s2: takes in another string
+ * @n: takes in an integer
+ * Return: returns a new string up to the num passed for str2
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *nstr, *empty = "";
+
+	unsigned int i, j, str1 = 0, str2 = 0;
+
+	if (s1 == NULL)
+		s1 = empty;
+	else
+		str1 = _strlen(s1);
+	if (s2 == NULL)
+		s2 = empty;
+	else
+		str2 = _strlen(s2);
+	if (n >= str2)
+		n = str2;
+	nstr = malloc((str1 * sizeof(char)) + n + 1);
+	if (nstr == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		nstr[i] = s1[i];
+	if (str2 < n)
+	{
+		for (j = 0; s2[j] != '\0'; j++, i++)
+		{
+			nstr[i] = s2[j];
+		}
+	}
+	else
+	{
+		for (j = 0; j < n; j++, i++)
+		{
+			nstr[i] = s2[j];
+		}
+	}
+	nstr[i] = '\0';
+	return (nstr);
 }
