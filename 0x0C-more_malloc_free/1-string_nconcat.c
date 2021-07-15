9feacd80c3dcf09e35e42e8951b452563b215a23
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-07-15 12:40:22 -0700

    All more malloc free files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,41 +4,43 @@
  */
 
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
- * *string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: limit of s2
- * Return: pointer to new space in memory or null
- **/
+ * string_nconcat - concatenates two strings
+ * @s1: destination string
+ * @s2: string to concatenates
+ * @n: number of bytes to copy from s2
+ * Return: pointer to new allocated memory
+ */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *strDup;
-	int i;
-	unsigned int j;
+	char *s;
+	unsigned int i, lens1 = 0, lens2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	strDup = malloc(sizeof(char) * (i + n + 1));
-	if (strDup == NULL)
-		return (NULL);
-	i = j = 0;
-	while (s1[i] != '\0')
-	{
-		strDup[i] = s1[i];
-		i++;
-	}
-	while (j < n && s2[j] != '\0')
-	{
-		strDup[i] = s2[j];
-		i++, j++;
-	}
-	strDup[i] = '\0';
-	return (strDup);
+
+	while (*(s1 + lens1) != '\0')
+		lens1++;
+	while (*(s2 + lens2) != '\0')
+		lens2++;
+
+	if (lens2 >= n)
+		lens2 = n;
+
+	s = malloc(lens1 + n + 1);
+	if (s == NULL)
+		return (s);
+
+	for (i = 0; i < lens1; i++)
+		s[i] = s1[i];
+	for (i = 0; i < n; i++)
+		s[i + lens1] = s2[i];
+	s[i + lens1] = '\0';
+	return (s);
 }
