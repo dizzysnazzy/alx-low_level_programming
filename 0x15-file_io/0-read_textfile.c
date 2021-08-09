2feab3279d053584654b4f4db170a3642791541c
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-08-09 05:09:30 -0700

    modify file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,12 @@
  */
 
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <stdlib.h>
+
 
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
@@ -17,38 +22,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
+	ssize_t o, r, w;
+	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	s = read(fd, buf, letters);
-	if (s < 0)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buf);
-		return (0);
-	}
-	buf[s] = '\0';
-
-	close(fd);
-
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
-	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
 
-	free(buf);
-	return (t);
+	free(buffer);
+	close(o);
+
+	return (w);
 }
