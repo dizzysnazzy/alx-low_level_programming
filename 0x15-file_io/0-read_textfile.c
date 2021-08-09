a3d5de5955e852ac7a9408455c06e7124742ce90
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-08-09 04:50:18 -0700

    modify file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,28 +17,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	int fd;
+	int s, t;
+	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
 
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
 
-	if (o == -1 || r == -1 || w == -1 || w != r)
+	s = read(fd, buf, letters);
+	if (s < 0)
 	{
-		free(buffer);
+		free(buf);
+		return (0);
+	}
+	buf[s] = '\0';
+
+	close(fd);
+
+	t = write(STDOUT_FILENO, buf, s);
+	if (t < 0)
+	{
+		free(buf);
 		return (0);
 	}
 
-	free(buffer);
-	close(o);
-
-	return (w);
+	free(buf);
+	return (t);
 }
