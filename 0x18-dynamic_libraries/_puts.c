035b8640aae87503799fb18c932239a1245b014c
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-09-20 13:14:30 -0700

    dynamic libraries file

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: _puts.c
+ * Auth: Dismas Kipchumba
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @s: The string to be printed.
+ */
+void _puts(char *s)
+{
+	while (*s)
+		_putchar(*s++);
+
+	_putchar('\n');
+}
