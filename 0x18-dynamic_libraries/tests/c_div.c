5c24890ceea7753778e7b064666f3976716de188
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-09-20 18:24:12 -0700

    dynamic libraries file

--- 0x18-dynamic_libraries/tests/c_div.c
+++ 0x18-dynamic_libraries/tests/c_div.c
@@ -1,18 +1,18 @@
 /*
- * File: c_divide.c
+ * File: c_div.c
  * Auth: Dismas Kipchumba
  */
 
 #include "holberton.h"
 
 /**
- * divide - divides two integers.
+ * div - divides two integers.
  * @a: The first integer to be divided.
  * @b: The second integer to be divided.
  *
  * Return: The result of the division.
  */
-int divide(int a, int b)
+int div(int a, int b)
 {
 	return (a / b);
 }
