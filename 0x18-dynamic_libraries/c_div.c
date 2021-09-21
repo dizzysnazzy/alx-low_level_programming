497a6553e811ed1346c60bdf795ecf01c0a700a2
dizzysnazzy <dismaskipchumba2@gmail.com>
2021-09-21 03:22:29 -0700

    dynamic libraries file

---
+++ 0x18-dynamic_libraries/c_div.c
@@ -0,0 +1,18 @@
+/*
+ * File: c_div.c
+ * Auth: Dismas Kipchumba
+ */
+
+/* #include "holberton.h" */
+
+/**
+ * div - divides two integers.
+ * @a: The first integer to be divided.
+ * @b: The second integer to be divided.
+ *
+ * Return: The result of the division.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
