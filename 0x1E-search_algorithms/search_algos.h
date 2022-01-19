99b2a3dc10a29140734caafd362860376215e314
dizzysnazzy <dismaskipchumba2@gmail.com>
2022-01-19 00:46:24 -0800

    mod file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -43,6 +43,7 @@ typedef struct skiplist_s
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
 int jump_search(int *array, size_t size, int value);
