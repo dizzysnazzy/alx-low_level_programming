b89846da8ea11eda399c3ff6c72157add3631eb9
dizzysnazzy <dismaskipchumba2@gmail.com>
2023-03-02 00:55:40 -0800

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,13 +14,12 @@
 
 void _puts(char *str)
 {
-	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-
+    int i = 0;
+    while (str[i])
+    {
+        putchar(str[i]);
+        i++;
+    }
 }
 
 /**
@@ -31,24 +30,24 @@ void _puts(char *str)
 
 int _atoi(const char *s)
 {
-	int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+    int sign = 1;
+    unsigned long int resp = 0, firstNum, i;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
+    for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+    {
+        if (s[firstNum] == '-')
+        {
+            sign *= -1;
+        }
+    }
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
+    for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+    {
+        resp *= 10;
+        resp += (s[i] - 48);
+    }
 
-	return (sign * resp);
+    return (sign * resp);
 }
 
 /**
@@ -59,18 +58,16 @@ int _atoi(const char *s)
 
 void print_int(unsigned long int n)
 {
+    unsigned  long int divisor = 1, i, resp;
 
-	unsigned  long int divisor = 1, i, resp;
+    for (i = 0; n / divisor > 9; i++, divisor *= 10)
+        ;
 
-	for (i = 0; n / divisor > 9; i++, divisor *= 10)
-		;
-
-	for (; divisor >= 1; n %= divisor, divisor /= 10)
-	{
-		resp = n / divisor;
-		_putchar('0' + resp);
-	}
-
+    for (; divisor >= 1; n %= divisor, divisor /= 10)
+    {
+        resp = n / divisor;
+        putchar('0' + resp);
+    }
 }
 
 /**
@@ -82,15 +79,29 @@ void print_int(unsigned long int n)
 
 int main(int argc, char const *argv[])
 {
-	(void)argc;
+    if (argc != 3)
+    {
+        _puts("Error ");
+        exit(98);
+    }
 
-	if (argc != 3)
-	{
-		_puts("Error ");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
-	_putchar('\n');
+    for (int i = 1; i < argc; i++)
+    {
+        for (int j = 0; argv[i][j]; j++)
+        {
+            if (argv[i][j] < 48 || argv[i][j] > 57)
+            {
+                _puts("Error\n");
+                exit(98);
+            }
+        }
+    }
 
-	return (0);
+    unsigned long int num1 = _atoi(argv[1]);
+    unsigned long int num2 = _atoi(argv[2]);
+
+    print_int(num1 * num2);
+    putchar('\n');
+
+    return 0;
 }
